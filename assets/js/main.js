f4ceddebbdd2b5d11ad5b74bcd6e47b3021ff35a
luucace <lucace_09@hotmail.com>
2024-09-20 13:26:11 -0300

    finish commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,100 +1,84 @@
 const pizzas = [ 
-{
-    id: 1,
-    name: "Calabrese",
-    precio: 900,
-    imagen: `<img src="/assets/img/1.jpg">`
+    { id: 1, name: "Calabrese", precio: 900, imagen: "../assets/img/1.jpg" }, 
+    { id: 2, name: "Cebollada", precio: 800, imagen: "../assets/img/2.jpg" },
+    { id: 3, name: "Albaca", precio: 750, imagen: "../assets/img/3.jpg" },
+    { id: 4, name: "Especial", precio: 700, imagen: "../assets/img/4.jpg" },
+    { id: 5, name: "Muzza", precio: 650, imagen: "../assets/img/5.jpg" },
+    { id: 6, name: "Napolitana", precio: 700, imagen: "../assets/img/6.jpg" }
+];
 
-}, 
-
-{
-    id: 2,
-    name: "cebollada",
-    precio: 800,
-    imagen: `<img src="/assets/img/2.jpg">`
-
-},
-
-{
-    id: 3,
-    name: "albaca",
-    precio: 750,
-    imagen: `<img src="/assets/img/3.jpg">`
-
-},
-
-{
-    id: 4,
-    name: "Especial",
-    precio: 700,
-    imagen: `<img src="/assets/img/4.jpg">`
-
-},
-
-{
-    id: 5,
-    name: "Muzza",
-    precio: 650,
-    imagen: `<img src="/assets/img/5.jpg">`
-
-},
-
-{
-    id: 6,
-    name: "napolitana",
-    precio: 700,
-    imagen: `<img src="/assets/img/6.jpg">`
-
-},
-]; 
+const taskForm = document.querySelector ('.add-form');
+const createCard = document.querySelector('#container-pizzas')
 
 
-const form = document.getElementById("Formulario");
-const resultado = document.getElementById("container-pizzas");
 
-
-// Renderizar la pizzas
-const renderPizzas = (pizzas) => {
-    resultado.innerHTML = `
-    <div class="card-pizza">
-        <h2> ${pizzas.name}</h2>
-        <img src="${pizzas.imagen}">
-        <p>Precio: $${pizzas.precio}</p>
-    </div>
-    `;
-};
-
-// Renderizar un mensaje de  error
-const renderError = (message) => {
-    resultado.innerHTML = `<div class="error">${message}</div>`;
+const createProductCard = (pizza) => {
+    const { imagen, precio, name } = pizza;
+    return `
+        <div class="product-card">
+            <h2>${name}</h2>
+            <img src="${imagen}" width="150"/>
+            <p>Precio: $${precio}</p>
+        </div>
+    `
 };
 
 
+const renderProductCard = (pizza) => {
+    createCard.innerHTML = createProductCard(pizza);
+};
 
-// Obtener id por  pizzas
-const pizza = pizzasId(inputValue);
 
-
-// Evento submit del formulario
-
-form.addEventListener("submit", (e) => {
+// Funcion submit del product 
+const handleFormSubmit = (e) => {
     e.preventDefault();
-    const inputValue = parseInt(document.getElementById("inputnumber").value);
 
 
-    // Validar si esta vacio el input 
-    if (!inputValue) {
-        renderError("Por favor ingrese un número");
-        return;
-    }
+    // Obtener el valor ingresado del id
+    const pizzaId = parseInt(document.getElementById ('pizza-id').value);
 
-    // Si se encuentra el id de la pizza se renderiza
-    const pizza = getElementById (pizzas);
-    
-    if (pizza) {
-        renderPizzas(pizza  );
+    // Buscar la pizza por id
+    const pizzaEncontrada = pizzas.find((pizza) => pizza.id === pizzaId);
+
+
+    // Si se encuentra la pizza la renderizamos, si no mostramos un mensaje
+
+    if (pizzaEncontrada) {
+        renderProductCard(pizzaEncontrada);
+
+    // localStorage 
+    localStorage.setItem('ultimaPizza', JSON.stringify(pizzaEncontrada));
+
+
     } else {
-        renderError("No hay ninguna pizza con ese número");
+        createCard.innerHTML = `<h3>No se encontro ninguna pizza con ese ID</h3>`;
     }
 
-});
+
+
+    // Limpiador de busquedas 
+    
+    document.getElementById ('pizza-id').value = '';
+}
+
+
+
+
+
+
+// Ultima pizza 
+    const renderUltPizza = () => {
+        const ultimaPizza = localStorage.getItem('ultimaPizza');
+        if (ultimaPizza) {
+            const pizza = JSON.parse(ultimaPizza);
+            renderProductCard(pizza);
+        }
+    }
+
+// funcion iniciadora
+const init = () => {
+    taskForm.addEventListener('submit', handleFormSubmit);
+}
+
+
+init ()
